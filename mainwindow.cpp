77ea6647a7466f09f092b30b3fbf3809dcff1aac
RBrNx <conor.watson94@gmail.com>
2016-02-01 00:11:06 +0000

    Added Basic GUI

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,19 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QTCore>
+#include <QtGui>
 
-MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
+MainWindow::MainWindow(QWidget *parent):QMainWindow(parent),ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    ui->spinner_RoomH->setRange(1,20);
+    ui->spinner_RoomW->setRange(1,20);
+    ui->spinner_Boxes->setRange(1,4);
+    ui->spinner_Levels->setRange(1,20);
+
+    ui->label_GenerationTime->setText("Estimated Generation Time: 0 Hours 0 Minutes");
+    ui->progressBar->setValue(0);
 }
 
 MainWindow::~MainWindow()
