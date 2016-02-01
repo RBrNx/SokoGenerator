49d866df1b7bb82eac7d5ba876ba2383120d91de
RBrNx <conor.watson94@gmail.com>
2016-02-01 23:31:27 +0000

    Program can now produce random variables from GUI. Program Icon changed. Progress bar now linked and working.

--- sokogenerator.cpp
+++ sokogenerator.cpp
@@ -1,31 +1,54 @@
 #include "sokogenerator.h"
+#include "mainwindow.h"
 #include <QMessageBox>
+#include <time.h>
 
+SokoGenerator::SokoGenerator(QObject *parent):QObject(parent){
+    roomHeight = 0;
+    roomWidth = 0;
+    noOfBoxes = 0;
+    noOfLevels = 0;
+    difficulty = 0;
 
-
-int SokoGenerator::roomWidth = 0;
-int SokoGenerator::roomHeight = 0;
-int SokoGenerator::noOfBoxes =0;
-int SokoGenerator::noOfLevels =0;
-int SokoGenerator::difficulty = 0;
-int SokoGenerator::percentage =0;
-
-
-
-SokoGenerator::SokoGenerator(){
-
+    percentage = 0;
 }
 
 SokoGenerator::~SokoGenerator(){
 
 }
 
-void SokoGenerator::generateLevel(){
-    generateLevel(noOfLevels);
+int SokoGenerator::randomNumber(int min, int max){
+    return rand() % (max-min)+min;
 }
 
-void SokoGenerator::generateLevel(int noOfLevels){
-    for(int i = 0; i < noOfLevels; i++){
-        QMessageBox::information(NULL, "Information", "No of Levels: " + QString::number(noOfLevels) + "\n Room Width: " + QString::number(roomWidth) + "\n Room Height: " + QString::number(roomHeight) + "\n No of Boxes: " + QString::number(noOfBoxes) + "\n Difficulty: " + QString::number(difficulty));
+void SokoGenerator::generateLevel(){
+    srand(time(NULL));
+    int _levels;
+
+    if(noOfLevels == 0){ _levels = randomNumber(1, 20); } else { _levels = noOfLevels; }
+
+    for(int i = 0; i < _levels; i++){
+        generateLevel(roomWidth, roomHeight, noOfBoxes, difficulty, i);
+        float perc = ((i + 1) * 100) / _levels;
+        updatePercentage(perc);
     }
+
+
+}
+
+void SokoGenerator::generateLevel(int roomWidth, int roomHeight, int noOfBoxes, int difficulty, int levelNumber){
+    int _roomW, _roomH, _Boxes, _difficulty;
+    srand(time(NULL));
+
+    if(roomWidth == 0){ _roomW = randomNumber(5, 20); } else { _roomW = roomWidth; }
+    if(roomHeight == 0){ _roomH = randomNumber(5, 20); } else { _roomH = roomHeight; }
+    if(noOfBoxes == 0){ _Boxes = randomNumber(1, 4); } else { _Boxes = noOfBoxes; }
+    if(difficulty == 0){ _difficulty = randomNumber(1, 5); } else { _difficulty = difficulty; }
+
+    QMessageBox::information(NULL, "Information", "Generated Level: " + QString::number(levelNumber) + "\n Room Width: " + QString::number(_roomW) + "\n Room Height: "
+                             + QString::number(_roomH) + "\n No of Boxes: " + QString::number(_Boxes) + "\n Difficulty: " + QString::number(_difficulty));
+}
+
+void SokoGenerator::updatePercentage(float value){
+    emit changeProgressBar(value);
 }
--- sokogenerator.h
+++ sokogenerator.h
@@ -1,30 +1,43 @@
 #ifndef SOKOGENERATOR_H
 #define SOKOGENERATOR_H
 
+#include <QObject>
 
-class SokoGenerator{
+
+class SokoGenerator : public QObject{
+    Q_OBJECT
+
 public:
-    SokoGenerator();
+    explicit SokoGenerator(QObject *parent = 0);
     ~SokoGenerator();
 
-    static void setRoomWidth(int value){ roomWidth = value; }
-    static void setRoomHeight(int value){ roomHeight = value; }
-    static void setBoxes(int value){ noOfBoxes = value; }
-    static void setLevels(int value){ noOfLevels = value; }
-    static void setDifficulty(int value){ difficulty = value; }
+    void setRoomWidth(int value){ roomWidth = value; }
+    void setRoomHeight(int value){ roomHeight = value; }
+    void setBoxes(int value){ noOfBoxes = value; }
+    void setLevels(int value){ noOfLevels = value; }
+    void setDifficulty(int value){ difficulty = value; }
+    void setPercentage(int value){ percentage = value; }
 
-    static int getPercentage(){ return percentage; }
+    void resetVariables(){ roomHeight = roomWidth = noOfBoxes = noOfLevels = difficulty = 0; }
 
-    static void generateLevel();
-    static void generateLevel(int noOfLevels);
+    int getPercentage(){ return percentage; }
+    void updatePercentage(float value);
+
+    void generateLevel();
+    void generateLevel(int roomWidth, int roomHeight, int noOfBoxes, int difficulty, int levelNumber);
+
+    int randomNumber(int min, int max);
 
 private:
-    static int roomWidth;
-    static int roomHeight;
-    static int noOfBoxes;
-    static int noOfLevels;
-    static int difficulty;
-    static int percentage;
+    int roomWidth;
+    int roomHeight;
+    int noOfBoxes;
+    int noOfLevels;
+    int difficulty;
+    int percentage;
+
+signals:
+    void changeProgressBar(float);
 
 };
 
